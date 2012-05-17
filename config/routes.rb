c5abd7ed89b7a27d148b4cbcab09c7795e0f2dc0
kurko <chavedomundo@gmail.com>
2012-05-17 00:29:24 -0300

    Admin header design in place

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Blog::Application.routes.draw do
   get "dashboard/index"
 
-  devise_for :admin_users
+  devise_for :admin_users, path: "admin",
+    controllers: {
+      sessions: "admin/sessions"
+    }
 
   namespace :admin do
 
