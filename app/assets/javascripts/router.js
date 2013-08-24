66c49c48311a6903a51bdf61916962489c042c56
Alexandre de Oliveira <chavedomundo@gmail.com>
2013-08-24 17:27:33 -0300

    Final touches on the about page

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,6 +1,7 @@
 // For more information see: http://emberjs.com/guides/routing/
 
 App.Router.map(function() {
+  this.resource('about');
   this.resource('texts', function() {
     this.route('show', { path: '/:text_id' });
   });
